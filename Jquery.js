4ed3b532ccd015a5b6b3008a6912a1a3da082890
Adam <adaost0110@hotmail.com>
2018-11-14 16:53:53 +0100

    More jquery developing, trying to implement some things

--- Jquery.js
+++ Jquery.js
@@ -1,10 +1,51 @@
-﻿$(function () {
+﻿let playerCard;
+let computerCard;
+let playerscore = 0;
 
-    var card = $("main div");
-    card.hide();
+$(function () {
+
+    let deckId;
+    const baseUrl = "https://deckofcardsapi.com/api/deck/"
 
     $("#newgame").on("click", function () {
-        card.show();
+        $.getJSON(baseUrl + "new/shuffle/")
+            .done(function (data) {
+                deckId = data.deck_id;
+                $.getJSON(baseUrl + deckId + "/draw/")
+                    .done(function (drawData) {
+                        setPlayerCard(drawData.cards[0]);
+
+                        setTimeout(function () {
+                            $.getJSON(baseUrl + deckId + "/draw/")
+                                .done(function (computerDraw) {
+                                    computerCard = computerDraw.cards[0].code;
+                                    $("#computer-deck img").attr("src", computerDraw.cards[0].image);
+                                });
+                        }, 1000);
+                    });
+            });
+    });
+
+    $("#player-deck").on("click", function () {
+        SetScore(computerCard, playerCard);
+        console.log(playerscore);
     });
 
 });
+
+function setPlayerCard(card) {
+    playerCard = card.code;
+    $("#player-deck img")
+        .attr("src", card.image);
+};
+
+
+function SetScore(computercard, playercard) {
+    if (computercard.charAt(0) === playercard.charAt(0) || computercard.charAt(1) === playercard.charAt(1)) {
+        playerscore += 1;
+    }
+    else {
+        playerscore += -1;
+    }
+    $("#gamepoints").text(playerscore);
+};
